1a8565d7d9b1f82e52e1009edaaf2e94c857520b
thayashi <thayashi@couprio.com>
2018-01-26 21:55:48 +0900

    Style for Future-shop

---
+++ src/js/modules/AddClass.js
@@ -0,0 +1,16 @@
+export default class AddClass {
+
+
+}
+
+
+// export default class AddClass {
+//   constructor () {
+//     this.rowOffcanvas = document.getElementsByClassName('layout_body');
+//     this.addNewClass();
+//   }
+//
+//   addNewClass () {
+//     this.rowOffcanvas.classList.add(' row-offcanvas row-offcanvas-left');
+//   }
+// }
